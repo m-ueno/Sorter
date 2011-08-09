ba21fe15bb27cabc1470d6ac84d46f30ade023ea
uenop@archvm.thinkpad <dummy@mail.com>
2011-08-09 22:01:01 +0900

    BubbleSortを実装

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,17 +1,9 @@
 package Sorter;
 
-use strict;
-use warnings;
-use 5.10.0;
-use Data::Dumper;
-local $Data::Dumper::Indent = 1;
-local $Data::Dumper::Terse = 1;
-
 sub new{
 #    bless {values=>[]},shift; #この1行で済ませられる
-    my $class = shift;
-    my $self = bless {values=>[1,2,3]}, $class;
-    return $self;
+    my ($class) = @_;
+    bless {values=>[]}, $class;
 }
 sub set_values{
     my ($self, @args) = @_;
@@ -19,7 +11,7 @@ sub set_values{
     return $self;
 }
 sub get_values{
-    my $self = shift;
+    my ($self) = @_;
     return @{ $self->{values} };          # デリファレンス=>配列
 }
 sub sort{
@@ -41,14 +33,44 @@ sub _sort{
     return (_sort(@l), $pivot, _sort(@r));
 }
 
-my $s = Sorter->new;
-say "initial value", Dumper $s->{values};
+package Sorter::BubbleSorter;
+our @ISA = qw(Sorter);  # メソッド探索が行われるようになる
+sub _sort{
+    return () if not @_;
+    my @nums = @_;
+    for( my $i=0;$i<@nums;$i++ ){
+        for( my $j=$i;$j<@nums;$j++ ){
+            if( $nums[$i] > $nums[$j] ){
+                my $temp = $nums[$i];
+                $nums[$i] = $nums[$j];
+                $nums[$j] = $temp;
+            }
+        }
+    }
+}
 
-$s->set_values(5,4,3,2,1,2,3,4,5);
+# ----------------------------------------------------------------
+package Main;
+use strict;
+use warnings;
+use 5.10.0;
+use Data::Dumper;
+local $Data::Dumper::Indent = 1;
+local $Data::Dumper::Terse = 1;
 
-say "get_values", Dumper $s->get_values();
-$s->sort();
+sub test{
+    my $s = Sorter::BubbleSorter->new;
+    say "initial value", Dumper $s->{values};
 
-my @ans = $s->get_values();
-say "get_values(after)", Dumper [@ans];
+    $s->set_values(5,4,3,2,1,2,3,4,5);
 
+    say "get_values", Dumper [$s->get_values()];
+    $s->sort();
+
+    my @ans = $s->get_values();
+    say "get_values(after)", Dumper [@ans];
+}
+
+test();
+
+1;
